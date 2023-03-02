b75b2847a27dd5f0682722fd2a3a9e0af42c8808
Diego Vásquez <vas211628@uvg.edu.gt>
2023-03-02 09:06:47 -0600

    Creación del proyecto

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+import java.util.HashMap;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, String> hashMap = new HashMap<>();
+        
+
+    }
+}
---
+++ src/iMap.java
@@ -0,0 +1,10 @@
+/**
+ * iMap
+ */
+public interface iMap {
+
+    public void HashMap();
+    public void TreeMap();
+    public void LinkedHashMap();
+
+}
